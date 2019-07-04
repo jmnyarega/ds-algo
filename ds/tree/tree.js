ecb9cd2c736279f110ea2d8250d816262ff94f59
jmnyarega <josiah.mokobo@andela.com>
2019-07-05 02:08:20 +0300

    configure eslint + BST datastructure

---
+++ ds/tree/tree.js
@@ -0,0 +1,184 @@
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  show() {
+   return this.data;
+  }
+
+}
+
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  // can be made better. This algorithm only inserts data on one child node :waaat ??
+  insert(data) {
+    const node = new Node(data, null, null);
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      let parent;
+      while (true) {
+        parent = current; // subtree parent
+        if (data < current.data) {
+          current = current.left; // moving parent...
+          if (current === null) {
+            parent.left = node;
+            break;
+          }
+        } else {
+          current = current.right;
+          if (current === null) {
+            parent.right = node;
+            break;
+          }
+        }
+      }
+    }
+  }
+  
+  /**
+   * Depth first search tree traversals                 
+   *  1. Inorder(Left, Root, Right) 4 2 5 1 3 
+   *  2. PreOrder(Root, Left, right) 1 2 4 5 3
+   *  3. PostOrder(Left, Right, Root) 4 5 2 3 1
+   *
+   *               1
+   *
+   *           2        3 
+   *
+   *        4    5   
+   *
+   * Breadth First Traversal/Level Order 1 2 3 4 5
+   */
+
+  inorder(root) {
+    /* InOrder use cases: 
+     * 1. Non-increasing order.
+     */
+    if (root) {
+      this.inorder(root.left); // Left
+      console.log(root.data); // Root
+      this.inorder(root.right) // Right
+    }
+  }
+
+  preorder(root) {
+    /*
+     * PreOrder Usecases:
+     *  - used to create a copy of a tree.
+     *  - Used to get prefix expression on if an expression. http://en.wikipedia.org/wiki/Polish_notation
+     */
+    if (root) {
+      console.log(root.data); // Root
+      this.preorder(root.left); // Left
+      this.preorder(root.right); // Right
+    }
+  }
+
+  postorder(root) {
+    /*
+     * PostOrder usecases:
+     * - Used to delete a tree. https://www.geeksforgeeks.org/write-a-c-program-to-delete-a-tree/
+     * - To get postfix of an expression tree. http://en.wikipedia.org/wiki/Reverse_Polish_notation
+     */
+    if (root) {
+      this.postorder(root.left); // left
+      this.postorder(root.right); // right
+      console.log(root.data); // root
+    }
+  }
+  
+  /**
+   * Traverse level by level
+   */
+  breadthFirstSearch() {}
+
+  /*
+   * You only need to traverse through the left child nodes. until you get to the last node.
+   */
+  getMin() {
+    let current = this.root;
+    while(current.left) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  /**
+   * The best ...
+   *
+   */
+  getMax() {
+    let current = this.root;
+    while(current.right) {
+      current = current.right;
+    }
+    return current.data;
+  }
+  
+  find(data) {
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current === null) {
+        return null;
+      }
+    }
+    return current;
+  }
+
+  // complex
+  removeNode(node, data) {
+    if(!node) return null;
+    if (data === node.data) {
+      // has no children
+      if (!node.left && !node.left) {
+        return null;
+      }
+      // has no left child
+      if (!node.left) {
+        return node.right;
+      }
+
+      // has no right child
+      if (!node.right) {
+        return node.left;
+      }
+
+      // node has two children
+      let leastElement = this.getMin(node.right);
+      node.data = leastElement;
+      node.right = this.removeNode(node.right, leastElement);
+      return node;
+    } else if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+      return node;
+    } else {
+      node.right = this.removeNode(node.right, data);
+      return node;
+    }
+  }
+
+  remove(data) {
+    root = this.removeNode(this.root, data);
+  }
+
+}
+
+/* @TODO @josiah
+ * - Add a function to the BST class that counts the number of nodes in a BST
+ * - Add a function to the BST class that counts the number of edges in a BST
+ * - Add a max function to the BST class that finds the max value in the BST
+ */
