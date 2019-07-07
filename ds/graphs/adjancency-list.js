ac0f6d432e4492dc89c62867382fbcdaeb474b29
jmnyarega <josiah.mokobo@andela.com>
2019-07-07 19:02:13 +0300

    fix formarting and variable namings

--- ds/graphs/adjancency-list.js
+++ ds/graphs/adjancency-list.js
@@ -1,4 +1,5 @@
 class Graph {
+
   constructor(vertices) {
     this.vertices = this.vertices;
     this.adj = [];
@@ -7,17 +8,18 @@ class Graph {
     }
   }
 
-  getAdjancies() {
+  showGraph() {
     return this.adj;
   }
 
   addEdge(v, w) {
     this.adj[v].push(w);
   }
+
 }
 
-const x = new Graph(10);
-x.addEdge(0, 1);
-x.addEdge(0, 2);
-x.addEdge(3, 4);
-console.log(x.getAdjancies());
+const graph = new Graph(10);
+graph.addEdge(0, 1);
+graph.addEdge(0, 2);
+graph.addEdge(3, 4);
+graph.showGraph() // returns graph
--- ds/graphs/adjancency-matrix.js
+++ ds/graphs/adjancency-matrix.js
@@ -3,24 +3,28 @@
  *
 */
 class Graph {
-	constructor() {
-		this.AdjencencyMatrix = Array(10);
-		for (let i = 0; i < 10; i++) {
-				this.AdjencencyMatrix[i] = Array(10);
-		}
-	}
-	getMatrix() {
-		return this.AdjencencyMatrix;
-	}
-	edges(i, j) {
-		this.AdjencencyMatrix[i][j] = true;
-		return this.AdjencencyMatrix;
-	}
+
+  constructor() {
+    this.Adj = Array(10);
+    for (let i = 0; i < 10; i++) {
+      this.AdjencencyMatrix[i] = Array(10);
+    }
+  }
+
+  addEdges(i, j) {
+    this.Adj[i][j] = true;
+    return this.Adj;
+  }
+
+  showGraph() {
+    return this.Adj;
+  }
+
 }
 
 const graph = new Graph();
-graph.edges(0, 1);
-graph.edges(1, 3);
-graph.edges(1, 4);
-graph.edges(2, 4);
-console.log(graph.getMatrix());
+graph.addEdges(0, 1);
+graph.addEdges(1, 3);
+graph.addEdges(1, 4);
+graph.addEdges(2, 4);
+graph.showGraph(); // constructed graph
