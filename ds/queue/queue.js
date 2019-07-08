3407285a6a9091f5d270035a6b776ded3dfe7df5
jmnyarega <josiah.mokobo@andela.com>
2019-07-08 18:52:41 +0300

    queue

---
+++ ds/queue/queue.js
@@ -0,0 +1,34 @@
+function Queue() {
+  this.dataStore = [];
+  this.enqueue = enqueue;
+  this.dequeue = dequeue;
+  this.front = front;
+  this.back = back;
+  this.toString = toString;
+  this.isEmpty = isEmpty; // returns a boolean
+}
+
+function enqueue(element) {
+  this.dataStore.push(element);
+}
+
+function dequeue() {
+  this.dataStore.shift();
+}
+
+function front() {
+  return this.dataStore[0];
+}
+
+function back() {
+  return this.dataStore[this.dataStore.length - 1];
+}
+
+function toString() {
+  return this.dataStore.join(',');
+}
+
+function isEmpty() {
+  if (this.dataStore.length === 0) return true;
+  return false;
+}
