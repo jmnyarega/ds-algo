9628214a7fc321422b07da5f24a8aae13dec7855
jmnyarega <josiah.mokobo@andela.com>
2019-07-12 02:11:41 +0300

    Simplify

--- applications/number-of-inversions-improved.js
+++ applications/number-of-inversions-improved.js
@@ -9,31 +9,25 @@ function countInv(A) {
     const mid = Math.floor(n / 2);
     const a = countInv(A.slice(0, mid));
     const b = countInv(A.slice(mid, n));
-    const mergeResult = merge(A.slice(0, mid), A.slice(mid, n));
-    const c = mergeResult[1];
+    const c = merge(A.slice(0, mid), A.slice(mid, n));
     return a + b + c;
   }
 }
 
 function merge(left, right) {
-  let result = [];
   let count = 0;
   let i = 0;
   let j = 0;
   let leftLen = left.length; 
   while(i < leftLen && j < right.length) {
     if (left[i] <= right[j]) {
-      result.push(left[i]);
       i += 1;
     } else {
-      result.push(right[j]);
       count += leftLen - i;
       j += 1;
     } 
   }
-  result += left.slice(i, left.length);
-  result += right.slice(j, right.length); 
-  return [result, count];
+  return count;
 }
 
-countInv([6,5,4,3,2,1]); // 15
+countInv([6,5,4,3,2,1]);
