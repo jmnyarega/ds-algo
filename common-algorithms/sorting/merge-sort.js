fc2e8c04e29d14ac2e3b53edd2de7b9f64b361b0
jmnyarega <josiah.mokobo@andela.com>
2019-07-11 13:01:39 +0300

    Implement merge-sort

--- common-algorithms/sorting/merge-sort.js
+++ common-algorithms/sorting/merge-sort.js
@@ -2,3 +2,31 @@
  * Study and implement merge sort algorithm
  *
  */
+
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+  else {
+    const mid = Math.floor(array.length / 2);
+    const firstHalf = array.slice(0, mid);
+    const secondHalf = array.slice(mid, array.length);
+    return merge(
+      mergeSort(firstHalf),
+      mergeSort(secondHalf)
+    );
+  }
+}
+
+function merge(array1, array2) {
+  let sortedArray = [];
+
+  while(array1.length && array2.length) {
+    if (array1[0] <= array2[0]) sortedArray.push(array1.shift());
+    else sortedArray.push(array2.shift());
+  }
+
+  while(array1.length) sortedArray.push(array1.shift());
+  while(array2.length) sortedArray.push(array2.shift());
+
+  return sortedArray;
+}
+console.log(mergeSort([1,2,1,2,30,20,12,3,23,23,90,1,0,4]))
