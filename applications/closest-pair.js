11a1e639b5335ec60239e796676b6a97ad6aa52c
jmnyarega <josiah.mokobo@andela.com>
2019-07-13 05:44:47 +0300

    Homework

---
+++ applications/closest-pair.js
@@ -0,0 +1,12 @@
+/*
+ * The closest pair problem concers points (x, y) E R^2 in the plane.
+ * To measure the distance between two points p1 = (x1, y1) and p2 = (x2, y2), - 
+ * we use Euclidean (straight - line) distance
+ * d(p1,p2) = square-root of ((x1 - x2)^2 + (y1 - y2)^2)
+ *
+ * @TODO study about this problem in depth & implement the solution in code.
+ *
+ * 1. Implement it using brute-force 
+ * 2. Use deivide and conquire
+ *
+ */
