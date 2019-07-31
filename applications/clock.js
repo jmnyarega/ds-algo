773186df8048b4e3e5e5f3c4e62a451aed7f7f10
jmnyarega <josiah.mokobo@andela.com>
2019-07-31 13:50:34 +0300

    remove console logs

--- applications/clock.js
+++ applications/clock.js
@@ -7,11 +7,9 @@ function clock(time) {
   const hour = timeArray[0] == 12 ? 0 : timeArray[0];
   const minute = timeArray[1] == 0 ? 60 : timeArray[1];
   const seconds = timeArray[2];
-  console.log()
   return ({
     hour: ((minute / 60) * 30) + (hour * 30),
     minute: (360 / 60) * minute,
     seconds: (360 / 60) * seconds
   });
 }
-console.log(clock("0:01:23"));
