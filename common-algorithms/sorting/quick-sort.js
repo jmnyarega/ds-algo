e0a67a903c62c730a758e0b115df42f86215d678
jmnyarega <josiah.mokobo@andela.com>
2019-08-07 18:58:32 +0300

    quicksort

--- common-algorithms/sorting/quick-sort.js
+++ common-algorithms/sorting/quick-sort.js
@@ -3,3 +3,36 @@
  * @TODO study and implement quick sort...
  *
  */
+
+function partition(A, startIndex, endIndex) {
+  const pivot = A[endIndex];
+  let leftIndex = startIndex;
+  let rightIndex = endIndex - 1;
+  while (leftIndex <= rightIndex) {
+    while (leftIndex <= endIndex && A[leftIndex] < pivot) { leftIndex++; }
+    while (rightIndex >= startIndex && A[rightIndex] > pivot) { rightIndex--; }
+
+    if (leftIndex < rightIndex) {
+      // swap
+      const temp = A[rightIndex];
+      A[rightIndex] = A[leftIndex];
+      A[leftIndex] = temp;
+    }
+    else {
+      const temp = A[endIndex];
+      A[endIndex] = A[leftIndex];
+      A[leftIndex] = temp;
+    }
+  }
+  return leftIndex;
+}
+
+function quickSort(A, startIndex, endIndex) {
+  if (startIndex >= endIndex) return;
+  const pivotIndex = partition(A, startIndex, endIndex);
+  quickSort(A, startIndex, pivotIndex - 1);
+  quickSort(A, pivotIndex + 1, endIndex);
+  return A;
+}
+
+quickSort([5, 1, 6, 8, 12, 0, 4], 0, [5, 1, 6, 8, 12, 0, 4].length - 1);
