ba96b6554c2998a6362b3439fa325464e5abd9cb
jmnyarega <josiah.mokobo@andela.com>
2019-12-01 13:32:12 +0300

    Array Arrangements

--- applications/Rearrange.java
+++ applications/Rearrange.java
@@ -23,9 +23,8 @@ public class Rearrange
     }
     System.out.println(Arrays.toString(arr));
   }
-
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     int arr[] = {-1, 2, -3, 4, 5, 6, -7, 8, -9, 0, 1, 2};
-		Rearrange.rearrangePositiveNegative(arr);
-	}
+    Rearrange.rearrangePositiveNegative(arr);
+  }
 }
