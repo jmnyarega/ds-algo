fb406336163c4cfed4fbd1e6ab757b916c447ccd
jmnyarega <josiah.mokobo@andela.com>
2019-12-29 10:31:10 +0300

    Smallest missing number

---
+++ applications/missing-number.js
@@ -0,0 +1,12 @@
+function missingNumber(arr, n, m) {
+  if (arr[0] !== 0) return 0;
+  for (let i = 0; i < arr.length; i++) {
+    if((arr[i+1] - arr[i]) > 1) {
+      return arr[i] + 1;
+    }
+  }
+}
+
+
+const missing = missingNumber([0, 1, 2, 6, 9], 5, 10);
+console.log(missing); // 3
